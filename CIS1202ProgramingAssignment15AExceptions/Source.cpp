f40346bc27a55740bc2267b44078b1d6eaae88f0
Leslie Glascoe <leslie.glascoe@sinclair.edu>
2021-04-27 13:56:02 -0400

    Added my try block into main to test my exceptions

--- CIS1202ProgramingAssignment15AExceptions/Source.cpp
+++ CIS1202ProgramingAssignment15AExceptions/Source.cpp
@@ -12,7 +12,14 @@ using namespace std;
 char character(char start, int offset);
 
 int main() {
+	char result;
+	try {
 
+		result = ("a", 1);
+	}
+	catch (string exception) {
+		cout << exception << endl;
+	}
 
 	system("pause");
 	return 0;
@@ -26,14 +33,17 @@ char character(char start, int offset) {
 		string exception = "Error! Invalid Character Exception";
 			throw exception;
 	}
-	if (result < 'A' || (result > 'Z') && (result < 'a' || (result > 'z')) {
+	if (result < 'A' || (result > 'Z') && (result < 'a' || (result > 'z')))
+	{
 
 		string exception = "Error! Invalid Range Exception";
 			throw exception;
 	}
 	if (!(isupper(start) == 0 || islower(result == 0))) {
+		
 		string exception ="Error! No upper to lower transitions allowed";
 			throw exception;
 	}
+	return result;
 }
 
